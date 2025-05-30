be97d8ebd1c32e8d1cbe134a979afe729f045634
James L <jameslilliott1997@gmail.com>
2025-05-30 22:23:24 +0100

    fix typo in shh -> ssh and add echos for progress tracking

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #/bin/bash
 
 # Update System
+echo "Update System"
 sudo apt update
 sudo apt upgrade -y
 
 # Firewall
-ufw allow shh
+echo "Configuring Firewall"
+ufw allow ssh
 ufw allow http
 ufw allow https
 ufw enable
 
 # Disable Password Login
+echo "Disabling Password Login"
 sed -i -e '/^\(#\|\)PasswordAuthentication/s/^.*$/PasswordAuthentication no/' /etc/ssh/sshd_config
 sed -i -e '/^\(#\|\)PubkeyAuthentication/s/^.*$/PubkeyAuthentication yes/' /etc/ssh/sshd_config
 sed -i -e '/^\(#\|\)PermitEmptyPasswords/s/^.*$/PermitEmptyPasswords no/' /etc/ssh/sshd_config
@@ -19,17 +22,23 @@ if ! grep -q "^ChallengeResponseAuthentication" /etc/ssh/sshd_config; then
 else 
     sed -i -e '/^\(#\|\)ChallengeResponseAuthentication/s/^.*$/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
 fi
-sudo systemctl reload ssh
 
 # Install fail2ban
+echo "Installing fail2ban"
 sudo apt install fail2ban
 
 # Install Postgres
+echo "Installing Postgres"
 sudo apt -y install postgresql postgresql-contrib
 sudo systemctl status postgresql
 
 # Install .Net
+echo ".net runtime"
 sudo apt install -y dotnet-runtime-8.0
 
 # Install nginx
+echo "nginx"
 sudo apt install -y nginx
+
+echo "rebooting"
+sudo reboot
