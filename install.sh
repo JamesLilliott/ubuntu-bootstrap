5f5d072b1a60fd8a1a14838c1bd0153bb37841cb
James L <jameslilliott1997@gmail.com>
2025-05-30 22:09:13 +0100

    add a few security tweaks

--- install.sh
+++ install.sh
@@ -2,21 +2,34 @@
 
 # Update System
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
+
+# Firewall
+ufw allow shh
+ufw allow http
+ufw allow https
+ufw enable
+
+# Disable Password Login
+sed -i -e '/^\(#\|\)PasswordAuthentication/s/^.*$/PasswordAuthentication no/' /etc/ssh/sshd_config
+sed -i -e '/^\(#\|\)PubkeyAuthentication/s/^.*$/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+sed -i -e '/^\(#\|\)PermitEmptyPasswords/s/^.*$/PermitEmptyPasswords no/' /etc/ssh/sshd_config
+if ! grep -q "^ChallengeResponseAuthentication" /etc/ssh/sshd_config; then
+    echo 'ChallengeResponseAuthentication no' >> /etc/ssh/sshd_config
+else 
+    sed -i -e '/^\(#\|\)ChallengeResponseAuthentication/s/^.*$/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+fi
+sudo systemctl reload ssh
+
+# Install fail2ban
+sudo apt install fail2ban
 
 # Install Postgres
 sudo apt -y install postgresql postgresql-contrib
 sudo systemctl status postgresql
-
 
 # Install .Net
 sudo apt install -y dotnet-runtime-8.0
 
 # Install nginx
 sudo apt install -y nginx
-
-# Firewall
-sudo ufw allow 80
-sudo ufw allow 443
-
-
